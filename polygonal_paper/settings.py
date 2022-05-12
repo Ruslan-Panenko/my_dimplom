2ea1a55ef1c831b516a064d42f81a84cf7b57249
Ruslan Panenko <panenko.ruslan1@gmail.com>
2022-05-12 14:36:27 +0300

    Diploy

--- polygonal_paper/settings.py
+++ polygonal_paper/settings.py
@@ -89,12 +89,6 @@ WSGI_APPLICATION = 'polygonal_paper.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
-import dj_database_url
-
-db_from_env = dj_database_url.config()
-DATABASE['default'].update(db_from_env)
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
